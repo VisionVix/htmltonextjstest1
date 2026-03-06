7517c62648e35b1800107948838fefaf7467fad7
Julian Neagu <neiulian@gmail.com>
2026-03-06 12:19:53 +0000

    Add app/layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,14 @@
+import './globals.css'
+export const metadata = { title: 'New Project-nextjs', description: 'Converted by VEX Studio' }
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <head>
+        <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:wght@400;600&family=IBM+Plex+Sans:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
+      </head>
+      <body>{children}
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js"></script>
+      </body>
+    </html>
+  )
+}
